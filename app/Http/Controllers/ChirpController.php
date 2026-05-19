4f28bc2066e2701395decb08b963049057d95988
Aditya kadia <adikadia05@gmail.com>
2026-05-19 08:48:27 +0530

    Completed Edit and delete chirps

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Chirp;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $chirps = Chirp::with('user')->latest()->take(50)->get();
+        return view('home',['chirps' => $chirps]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validate the incoming request data
+        $validated = $request->validate([
+            'message' => 'required|string|max:255|min:5',
+        ],
+        [
+            'message.required' => 'Please write something to Chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
+            'message.min' => 'Chirps must be at least 5 characters long.',
+        ]
+        );
+
+        //create the chirp (for anonymous users, user_id will be null)
+        Chirp::create([
+            'message' => $validated['message'],
+            'user_id' => null,
+        ]);
+
+        return redirect('/')->with('success', 'Chirp sent!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        return view('chirps.edit', compact('chirp'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+    // Validate the incoming request data
+        $validated = $request->validate([
+            'message' => 'required|string|max:255|min:5',
+        ],
+        [
+            'message.required' => 'Please write something to Chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
+            'message.min' => 'Chirps must be at least 5 characters long.',
+        ]
+        );
+
+        //Update the chirp
+        $chirp->update($validated);
+
+        return redirect('/')->with('success', 'Chirp updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        // $this->authorize('delete', $chirp);
+        $chirp->delete();
+        return redirect('/')->with('success', 'Chirp deleted!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ChirpController::class, 'index']);
+Route::post('/chirps', [ChirpController::class, 'store']);
+Route::get('/chirps/{chirp}/edit',[ChirpController::class, 'edit']);
+Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+//Registration Routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+Route::post('/register', RegisterController::class)
+    ->middleware('guest');
+Route::post('/logout',LogoutController::class)
+    ->middleware('auth');
